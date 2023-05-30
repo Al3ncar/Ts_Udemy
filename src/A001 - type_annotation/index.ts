a982f4fb93aae2df6165d505cf730fce1be24edb
Al3ncar <alencar29htcss@gmail.com>
2023-05-29 23:40:57 -0300

    Concluid configs

---
+++ src/A001 - type_annotation/index.ts
@@ -0,0 +1,21 @@
+/* eslind-disable */
+
+const nome: string = "Igor";
+const idade: number = 18;
+const adulto: boolean = true;
+const simbolo: symbol = Symbol('qualquer-simbol');
+// const big: bigint = 10n;
+
+/*
+  Tipos Básicos:
+    string: "", ``, '';
+    number: 10, 1.57, -5.55, 0xf00d, 0b1010, 0o7744;
+    boolean: restritamente true ou false;
+    symbol: symbol
+    bigint: bigint
+
+*/
+
+// No boolean só é true or false, não será aceito:
+// true: qualquer tipo de valor
+// false: "", 0, entre outros fatores.
