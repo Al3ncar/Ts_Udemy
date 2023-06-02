75205592a6fb03d16a2ac3df41ddb17809c2fd28
Al3ncar <alencar29htcss@gmail.com>
2023-06-02 09:42:15 -0300

    type anottations

--- src/Aula01/type_anottation.ts
+++ src/Aula01/type_anottation.ts
@@ -3,11 +3,12 @@
 
 /* ---- Tipos Primitivos ---- */
 
-const nome: string = "Igor";
+const nome: string = "Igor Alencar";
 const idade: number = 18;
 const adulto: boolean = true;
-const simbolo: symbol = Symbol('qualquer-simbol');
+const simbolo: symbol = Symbol('qualquer symbol')
 // const big: bigint = 10n;
+
 
 /*
   Tipos Básicos:
@@ -27,23 +28,31 @@ const simbolo: symbol = Symbol('qualquer-simbol');
 
 
 // Array
-    let ArrayNumber: Array<number> = [1, 2, 3];
-    let ArrayNumber2: number[] = [4, 5, 6];
 
-    let ArrayString: Array<string> = ['a', 'b','c'];
-    let ArrayString2: string[] = ['e', 'f','g'];
+    let ArrayNumber: Array<number> = [1, 2, 3];
+    let ArrayNumber2: number[] = [4, 5, 6]
+
+    let ArrayString: Array<string> = ["A", "B", "C"]
+    let ArrayString2: string[] = ["D", "E", "F"]
 
 // Object
     let pessoas: {nome: string, idade: number, adulto?: boolean} = {
-        nome: "Igor Alenar",
+        nome: "Igor Alencar",
         idade: 18,
     }
 
+    let pessoas2: {nome: string, idade: number, adulto?: boolean} = {
+        nome: "Igor Alencar", idade: 18,
+    }
 
 // function
     function soma(x: number, y:number):number {return x + y;};
     const result = soma( 18 , 2 );
 
+    function soma2 (x: number, y: number):number {return x + y};
+    const result2 = soma(20, 30);
+
     // function inteira
 
-    const soma2: (x:number, y:number) => number = (x,y) => x = y;
+    const soma3: (x:number, y:number) => number = (x, y) => x + y;
+    const soma4: (x:number, y:number) => number = (x, y) => x + y;
