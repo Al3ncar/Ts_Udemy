973f974c8e2853c60f4ead943c2c34a78ca79f96
Al3ncar <alencar29htcss@gmail.com>
2023-06-02 09:29:57 -0300

    type any

---
+++ src/Aula02/type_any.ts
@@ -0,0 +1,6 @@
+// UTILIZE ANY EM ULTIMO CASO 
+const returnMsg = (msg: any) => {return msg;};
+
+console.log(returnMsg([1, 2, 3]));
+console.log(returnMsg("Olá, tudo bem??"));
+console.log(returnMsg(1));
