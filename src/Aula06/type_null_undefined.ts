51e09d79909f3d5718e98096cff280d3ecd8264b
Al3ncar <alencar29htcss@gmail.com>
2023-06-08 19:25:47 -0300

    Type null and undefined

---
+++ src/Aula06/type_null_undefined.ts
@@ -0,0 +1,21 @@
+let x;
+if(typeof x === 'undefined') x = 20;
+console.log(x * 2);
+
+export function createPerson (
+    // Declarando parametro
+        firstName: string,
+        lastName?: string, // parametro opcional ( string / undefined )
+):{
+    // Declarando retorno de tipo
+        firstName: string,
+        lastName?: string, // parametro opcional ( string / undefined )
+} {
+    // Declarando retorno da função
+    return {
+        firstName,
+        lastName
+    }
+};
+
+// Quando o '?' é atribuido a uma tipo deixamos ele opcional
