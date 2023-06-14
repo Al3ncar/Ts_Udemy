fbbe79ac2258a31407bc508a51f6b57c510b5fb2
Al3ncar <alencar29htcss@gmail.com>
2023-06-14 10:39:27 -0300

    Type null and udefined

--- src/Aula06/type_null_undefined.ts
+++ src/Aula06/type_null_undefined.ts
@@ -23,4 +23,13 @@ export function createPerson (
 export function squareOf(x: any){
     if(typeof x === 'number') return x * x;
     return null
-}; ''
+};
+
+const squareOfNumber = squareOf(2);
+const squareOfString = squareOf("2");
+
+if(squareOfNumber === null){
+    console.log('Operação invalida');
+} else {
+    console.log(squareOfNumber)
+}
