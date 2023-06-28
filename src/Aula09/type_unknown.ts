bbe91a89e9228a6460eabb23d703416fa446db5e
Al3ncar <alencar29htcss@gmail.com>
2023-06-28 08:59:09 -0300

    Type Unknown

--- src/Aula09/type_unknown.ts
+++ src/Aula09/type_unknown.ts
@@ -1,7 +1,7 @@
 // Tipo unknown
 
 // Unknown é o pai de todos os tipos, ele veio primeiro do que os demias
-// unknown é bem mais seguro do que o enum
+// unknown é bem mais seguro do que o any
 
 let x: unknown;
 
@@ -17,3 +17,12 @@ let y = 100;
 // porem se criarmos uma condição ela executa
 
 if(typeof x === 'number') console.log(x + y);
+
+// unknown é um any mais seguro
+// ele vai te forçar a fazer a checagem dos tipos,
+// antes de excutar qualquer operção.
+
+
+// Sempre que voce precisa receber alguma coisa
+// que voce não sabe com antecendencia o que aquela coisa vai ser
+// inves de usar any use unknown  
