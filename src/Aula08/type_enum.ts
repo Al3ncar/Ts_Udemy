d3431a200290e565f5c9456c2412cc925762b712
Al3ncar <alencar29htcss@gmail.com>
2023-06-27 23:14:57 -0300

    Type Enum

---
+++ src/Aula08/type_enum.ts
@@ -0,0 +1,18 @@
+// Enum
+
+// Enum é um tipo que não existe em JavaScript, 
+// existe em outras linguagem como python, C#, Java
+
+// É uma estrutura de dados não ordenada
+// geralmente é usado quando temos mais de uma opção 
+
+enum Cores {
+    VERMELHO,
+    ROXO,
+    AMARELO
+}   
+
+console.log(Cores.AMARELO) // 2
+console.log(Cores.ROXO) // 1
+console.log(Cores.VERMELHO) // 0
+
