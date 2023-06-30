d70dfc99b7cd0ba2ece8a260e77e52fd3898488b
Al3ncar <alencar29htcss@gmail.com>
2023-06-30 07:07:09 -0300

    Type Null and Undefined

--- src/Aula06/type_null_and_undefined.ts
+++ src/Aula06/type_null_and_undefined.ts
@@ -1,15 +1,15 @@
 let x;
-if(typeof x === 'undefined') x = 20;
+if (typeof x === 'undefined') x = 20;
 console.log(x * 2);
 
-export function createPerson (
+export function createPerson(
     // Declarando parametro
-        firstName: string,
-        lastName?: string, // parametro opcional ( string / undefined )
-):{
+    firstName: string,
+    lastName?: string, // parametro opcional ( string / undefined )
+): {
     // Declarando retorno de tipo
-        firstName: string,
-        lastName?: string, // parametro opcional ( string / undefined )
+    firstName: string,
+    lastName?: string, // parametro opcional ( string / undefined )
 } {
     // Declarando retorno da função
     return {
@@ -20,15 +20,15 @@ export function createPerson (
 
 // Quando o '?' é atribuido a uma tipo deixamos ele opcional
 
-export function squareOf(x: any){
-    if(typeof x === 'number') return x * x;
+export function squareOf(x: any) {
+    if (typeof x === 'number') return x * x;
     return null
 };
 
 const squareOfNumber = squareOf(2);
 const squareOfString = squareOf("2");
 
-if(squareOfNumber === null){
+if (squareOfNumber === null) {
     console.log('Operação invalida');
 } else {
     console.log(squareOfNumber)
