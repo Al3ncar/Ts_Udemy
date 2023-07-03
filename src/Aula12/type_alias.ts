1b521037e724aa0988b3443f15e7c54b7fd4f4dd
Al3ncar <alencar29htcss@gmail.com>
2023-07-02 22:41:04 -0300

    Type Alias

--- src/Aula12/type_alias.ts
+++ src/Aula12/type_alias.ts
@@ -9,37 +9,32 @@ type People = {
     name: string,
     idade: Idade,
     salario: number,
-    corPreferida?: string 
+    corPreferida?: string
 }
-
 
 // Qualquer objeto que for do tipo pessoa terá os seguinte elementos
 
 // name: string,
 // idade: Idade === number,
- 
-type CorRGB = 'Vermelho' | 'Verde' | 'Azul'  
+
+type CorRGB = 'Vermelho' | 'Verde' | 'Azul'
 type CorCMYK = 'Ciano' | 'Magenta' | 'Amarelo' | 'Preto'
 
 type CorPreferida = CorRGB | CorCMYK;
 
 // Para que o tipo seja inserido no meu objeto basta colocar
 // na frente do nome da const|let os ":" com o nome do tipo 
-// que voce crio.
-
-
-// ex: 
+// que voce crio, ex: 
 
 // em TS quando quisermos separa um numero grande podemos usar o "_"
 const people: People = {
-    name:'Maria',
+    name: 'Maria',
     idade: idade,
     salario: 200_000_000 // 200000000
 }
 
-
-export function setCorPreferida(people: People, cor: CorPreferida): People{
-    return {...people, corPreferida: cor}
+export function setCorPreferida(people: People, cor: CorPreferida): People {
+    return { ...people, corPreferida: cor }
 }
 
 console.log(setCorPreferida(people, 'Ciano'))
